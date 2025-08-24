f574d498740c137ff780621f061668a68e9cd439
luispiamba97 <fernandopiamba97@gmail.com>
2025-08-24 17:05:49 -0500

    cree clase presentarse y mostrar estudiante

--- ejercicioAcademico/src/Estudiante.java
+++ ejercicioAcademico/src/Estudiante.java
@@ -3,13 +3,33 @@ public class Estudiante{
 
     private int idEstudiante;
     private String nombreEstudiante;
-    public String semestreActual;
+    public int semestreActual;
 
     // constructor
 
-    public Estudiante(int idEstudiante, String nombreEString, String semestreActual){
+    public Estudiante(int idEstudiante, String nombreEString, int semestreActual)
+    {
         this.idEstudiante = idEstudiante;
         this.nombreEstudiante = nombreEstudiante;
         this.semestreActual = semestreActual;
     }
+
+    // Metodos
+
+    public void mostrarEstudiante()  //Metodo para mostrar estudiante
+    {
+        System.out.println("Identificacion: " +idEstudiante);
+        System.out.println("Nombre: " +nombreEstudiante);
+        System.out.println("Semestre: " +semestreActual);
+    }
+
+    public void presentarEstudiante()  //Metodo para presentar estudiante
+    {
+        System.out.println("El estudiante " + nombreEstudiante + " ,identificad@ con: " + idEstudiante + ", y cursa actualmente: " + 
+        semestreActual + " Semestre");
+    }
+
+    
+
+
 }
